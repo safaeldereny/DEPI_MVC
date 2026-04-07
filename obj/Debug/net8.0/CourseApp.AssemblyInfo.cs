c30d0f9c27a98133c6906e4df5694bcfd43fcb55
Safaeldereny <your_email@example.com>
2026-04-07 17:48:19 +0200

    Add DEPI_MVC project for Day06

--- obj/Debug/net8.0/CourseApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/CourseApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bb2013795c3de322844ca657c2939ab3fcf967f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1d215d0baba9dc1ea013dbc9c1f46527336ba70")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
