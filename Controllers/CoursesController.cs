b1d215d0baba9dc1ea013dbc9c1f46527336ba70
Safaeldereny <your_email@example.com>
2026-04-07 14:53:11 +0200

    Add DEPI_MVC project for Day06

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CourseApp.Data; 
+using CourseApp.Models;
+
+
+public class CoursesController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public CoursesController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var courses = _context.Courses.ToList();
+        return View(courses);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Course course)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Courses.Add(course);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(course);
+    }
+
+    public IActionResult Edit(int id)
+    {
+        var course = _context.Courses.Find(id);
+        if (course == null)
+            return NotFound();
+
+        return View(course);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, Course course)
+    {
+        if (id != course.Id)
+            return NotFound();
+
+        if (ModelState.IsValid)
+        {
+            _context.Update(course);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(course);
+    }
+
+    public IActionResult Delete(int id)
+    {
+        var course = _context.Courses.Find(id);
+        if (course == null)
+            return NotFound();
+
+        return View(course);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        var course = _context.Courses.Find(id);
+        _context.Courses.Remove(course);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Details(int id)
+    {
+        var course = _context.Courses.Find(id);
+        if (course == null)
+            return NotFound();
+
+        return View(course);
+    }
+}
---
+++ Data/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CourseApp.Models;
+namespace CourseApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
